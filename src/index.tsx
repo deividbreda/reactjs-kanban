90d724aa66b8dc32dac880d3037503caa0bd0633
Deivid Breda <deivid.breda02@gmail.com>
2022-05-28 16:23:46 -0300

    Implementando fake back-end MirageJs

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,35 @@
+import { createServer, Model } from 'miragejs';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
+
+createServer({
+  models: {
+    fazer: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      fazeres: [
+        
+      ]
+    })
+  },
+
+  routes(){
+    this.namespace = 'api';
+
+    this.get('kanban/fazers', () => {
+      return this.schema.all('fazer');
+    })
+
+    this.post('kanban/fazers', (schema, request) => {
+      const data = JSON.parse(request.requestBody)
+
+      return schema.create('fazer', data)
+    })
+  }
+})
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
